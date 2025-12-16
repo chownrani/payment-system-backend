a60e73c39728763b6c05a0c6046f7caf4ff0b2d9
chownrani <chownrani@proton.me>
2025-12-15 21:34:44 -0300

    initial commit

---
+++ payment-system-backend/src/main/java/dev/raniere/payment_system_backend/PaymentSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.raniere.payment_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSystemBackendApplication.class, args);
+	}
+
+}
---
+++ payment-system-backend/src/test/java/dev/raniere/payment_system_backend/PaymentSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.raniere.payment_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
